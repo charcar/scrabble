17fa7256fe0ba52c203902ec5bb49a324b61b9a6
Charles Carter and Jacob Steinberg <charles.h.carter@gmail.com>
2016-02-09 10:02:19 -0800

    refactored main function from for loops to hashmap

--- scrabble/src/main/java/App.java
+++ scrabble/src/main/java/App.java
@@ -1,4 +1,5 @@
 import java.io.Console;
+import java.util.HashMap;
 
 public class App {
   public static void main(String[] args) {
@@ -9,45 +10,54 @@ public class App {
   }
 
     public Integer scrabbleScore(String userString) {
-      char[] userCharArray = userString.toCharArray();
+      char[] userCharArray = userString.toLowerCase().toCharArray();
       Integer totalScore = 0;
+
+      HashMap<Character, Integer> scoreHash = new HashMap<Character, Integer>();
+          scoreHash.put('a', 1);
+          scoreHash.put('e', 1);
+          scoreHash.put('i', 1);
+          scoreHash.put('o', 1);
+          scoreHash.put('u', 1);
+          scoreHash.put('r', 1);
+          scoreHash.put('l', 1);
+          scoreHash.put('s', 1);
+          scoreHash.put('t', 1);
+          scoreHash.put('n', 1);
+          scoreHash.put('d', 2);
+          scoreHash.put('g', 2);
+          scoreHash.put('b', 3);
+          scoreHash.put('c', 3);
+          scoreHash.put('m', 3);
+          scoreHash.put('p', 3);
+          scoreHash.put('f', 4);
+          scoreHash.put('h', 4);
+          scoreHash.put('v', 4);
+          scoreHash.put('w', 4);
+          scoreHash.put('y', 4);
+          scoreHash.put('k', 5);
+          scoreHash.put('j', 8);
+          scoreHash.put('x', 8);
+          scoreHash.put('q', 10);
+          scoreHash.put('z', 10);
+
       for (char eachChar : userCharArray ) {
-        if (eachChar == 'a' || eachChar == 'e'|| eachChar == 'i' || eachChar == 'o' || eachChar == 'u' || eachChar == 'l' || eachChar == 'r' || eachChar == 's' || eachChar == 't' || eachChar == 'n') {
-          totalScore = totalScore + 1;
-        } else if (eachChar == 'd' || eachChar == 'g') {
-          totalScore = totalScore + 2;
-        } else if (eachChar == 'b' || eachChar == 'c' || eachChar == 'm' || eachChar == 'p') {
-        totalScore = totalScore + 3;
-        } else if (eachChar == 'f' || eachChar == 'h' || eachChar == 'v' ||           eachChar == 'w' || eachChar == 'y') {
-        totalScore = totalScore + 4;
-       }
-     } return totalScore;
+        Integer letterValue = scoreHash.get(eachChar);
+        totalScore = totalScore + letterValue;
+      } return totalScore;
     }
   }
 
 
-// public static String leetspeak(String normalInput) {
-//   String[] wordsArray = normalInput.split(" ");
-//   String newWord = "";
-//   for (String wordsInArray : wordsArray) {
-//     wordsInArray = wordsInArray.replaceAll("e", "3");
-//     wordsInArray = wordsInArray.replaceAll("E", "3");
-//     wordsInArray = wordsInArray.replaceAll("o", "0");
-//     wordsInArray = wordsInArray.replaceAll("O", "0");
-//     wordsInArray = wordsInArray.replaceAll("I", "1");
-//     if (wordsInArray.startsWith("s")) {
-//       wordsInArray = wordsInArray.replaceAll("s", "z");
-//       wordsInArray = wordsInArray.replaceFirst("z", "s");
-//     } else if (wordsInArray.startsWith("S")) {
-//       wordsInArray = wordsInArray.replaceAll("S", "Z");
-//       wordsInArray = wordsInArray.replaceFirst("Z", "S");
-//       wordsInArray = wordsInArray.replaceAll("s", "z");
-//     } else {
-//       wordsInArray = wordsInArray.replaceAll("s", "z");
-//       wordsInArray = wordsInArray.replaceAll("S", "Z");
-//       }
-//       newWord = newWord.concat(" " + wordsInArray);
-//       newWord = newWord.trim();
-//     } return newWord;
-//   }
-// }
+  //       if (eachChar == 'a' || eachChar == 'e'|| eachChar == 'i' || eachChar == 'o' || eachChar == 'u' || eachChar == 'l' || eachChar == 'r' || eachChar == 's' || eachChar == 't' || eachChar == 'n') {
+  //         totalScore = totalScore + 1;
+  //       } else if (eachChar == 'd' || eachChar == 'g') {
+  //         totalScore = totalScore + 2;
+  //       } else if (eachChar == 'b' || eachChar == 'c' || eachChar == 'm' || eachChar == 'p') {
+  //       totalScore = totalScore + 3;
+  //       } else if (eachChar == 'f' || eachChar == 'h' || eachChar == 'v' ||           eachChar == 'w' || eachChar == 'y') {
+  //       totalScore = totalScore + 4;
+  //      }
+  //    } return totalScore;
+  //   }
+  // }

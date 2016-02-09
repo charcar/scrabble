7e5a3d65449fb91b7791cdf4c0ff6ac971379280
Charles Carter and Jacob Steinberg <student@epicodus.com>
2016-02-09 08:18:53 -0800

    initialized local files for scrabble, except from web utilities

---
+++ scrabble/src/main/java/App.java
@@ -0,0 +1,12 @@
+import java.io.Console;
+
+public class App {
+  public static void main(String[] args) {
+    Console myConsole = System.console();
+    System.out.println("Enter a Word and we'll calculate the Scrabble Score.");
+    String userString = myConsole.readLine();
+
+
+
+
+  }
---
+++ scrabble/src/test/java/AppTest.java
@@ -0,0 +1,20 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class AppTest {
+
+
+}
+
+//
+//   @Test
+//   public void leetspeak_returnsAStringAsIsWhenNoLeetspeakIsPresent_wordDoesntChange() {
+//     App yourApp = new App();
+//     assertEquals("happy", yourApp.leetspeak("happy"));
+//   }
+//
+//   @Test
+//   public void leetspeak_replacesEveryEInAStringWithA3_wordChanges() {
+//     App yourApp = new App();
+//     assertEquals("3l3phant", yourApp.leetspeak("Elephant"));
+// }

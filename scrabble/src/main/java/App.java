0dbb4768445ccb3302ea9ec0bbcf53e4f89d688b
Charles Carter and Jacob Steinberg <charles.h.carter@gmail.com>
2016-02-09 09:16:14 -0800

    pass first two specs, in need of serious refactoring

--- scrabble/src/main/java/App.java
+++ scrabble/src/main/java/App.java
@@ -2,11 +2,48 @@ import java.io.Console;
 
 public class App {
   public static void main(String[] args) {
-    Console myConsole = System.console();
-    System.out.println("Enter a Word and we'll calculate the Scrabble Score.");
-    String userString = myConsole.readLine();
-
-
-
-
+    // Console myConsole = System.console();
+    // System.out.println("Enter a Word and we'll calculate the Scrabble Score.");
+    // String userString = myConsole.readLine();
+    // System.out.println(leetspeak(userString));
   }
+
+    public Integer scrabbleScore(String userString) {
+      char[] userCharArray = userString.toCharArray();
+      Integer totalScore = 0;
+      for (char eachChar : userCharArray ) {
+        if (eachChar == 'a' || eachChar == 'e'|| eachChar == 'i' || eachChar == 'o' || eachChar == 'u' || eachChar == 'l' || eachChar == 'r' || eachChar == 's' || eachChar == 't' || eachChar == 'n') {
+          totalScore = totalScore + 1;
+        } else if (eachChar == 'd' || eachChar == 'g') {
+          totalScore = totalScore + 2;
+        }
+       } return totalScore;
+     }
+    }
+
+
+// public static String leetspeak(String normalInput) {
+//   String[] wordsArray = normalInput.split(" ");
+//   String newWord = "";
+//   for (String wordsInArray : wordsArray) {
+//     wordsInArray = wordsInArray.replaceAll("e", "3");
+//     wordsInArray = wordsInArray.replaceAll("E", "3");
+//     wordsInArray = wordsInArray.replaceAll("o", "0");
+//     wordsInArray = wordsInArray.replaceAll("O", "0");
+//     wordsInArray = wordsInArray.replaceAll("I", "1");
+//     if (wordsInArray.startsWith("s")) {
+//       wordsInArray = wordsInArray.replaceAll("s", "z");
+//       wordsInArray = wordsInArray.replaceFirst("z", "s");
+//     } else if (wordsInArray.startsWith("S")) {
+//       wordsInArray = wordsInArray.replaceAll("S", "Z");
+//       wordsInArray = wordsInArray.replaceFirst("Z", "S");
+//       wordsInArray = wordsInArray.replaceAll("s", "z");
+//     } else {
+//       wordsInArray = wordsInArray.replaceAll("s", "z");
+//       wordsInArray = wordsInArray.replaceAll("S", "Z");
+//       }
+//       newWord = newWord.concat(" " + wordsInArray);
+//       newWord = newWord.trim();
+//     } return newWord;
+//   }
+// }
--- scrabble/src/test/java/AppTest.java
+++ scrabble/src/test/java/AppTest.java
@@ -3,18 +3,16 @@ import static org.junit.Assert.*;
 
 public class AppTest {
 
-
+  @Test
+  public void scrabbleScore_returnsAScrabbleScoreForALetter_1() {
+    App app = new App();
+    Integer score = 1;
+    assertEquals(score, app.scrabbleScore("a"));
+  }
+  @Test
+  public void scrabbleScore_returnsAScrabbleScoreForALetter_2() {
+    App app = new App();
+    Integer score = 2;
+    assertEquals(score, app.scrabbleScore("d"));
 }
-
-//
-//   @Test
-//   public void leetspeak_returnsAStringAsIsWhenNoLeetspeakIsPresent_wordDoesntChange() {
-//     App yourApp = new App();
-//     assertEquals("happy", yourApp.leetspeak("happy"));
-//   }
-//
-//   @Test
-//   public void leetspeak_replacesEveryEInAStringWithA3_wordChanges() {
-//     App yourApp = new App();
-//     assertEquals("3l3phant", yourApp.leetspeak("Elephant"));
-// }
+}
